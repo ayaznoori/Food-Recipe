56e8e04f860eee80dceae8b89b9b1046dea129d4
ayaznoori <ayaz.noori15@gmail.com>
2022-04-28 17:47:07 +0530

    pushing

---
+++ script/navbar.js
@@ -0,0 +1,11 @@
+function navbar(){
+    return  `<div id="left">
+    <a href="index.html"> Home</a>
+ </div>
+ <div id="right">
+  <a href="login.html">Login</a>&emsp;
+  <a href="signup.html">Sign Up</a>
+  </div>`;
+  
+}
+export {navbar};
